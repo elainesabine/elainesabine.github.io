178f91c90c93bc5c9777bd8c5106dc36d5b3f4a4
Elaine Pranadjaya <elpranad@amazon.com>
2020-09-22 23:04:50 -0700

    fix clubs

--- elainesabine.js
+++ elainesabine.js
@@ -5,9 +5,9 @@
 // });
 
 
-// $(window).on('load',function () {
-//     $(".se-pre-con").fadeOut('slow');
-// });
+$(window).on('load',function () {
+    $(".se-pre-con").fadeOut('slow');
+});
 
 
 // clicking on the navbar buttons
